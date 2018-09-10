5b1a6725858362f0b7ec9c75dce049e6e7e7637d
Philip Patton <ppatton35745@gmail.com>
2018-09-10 14:31:51 -0500

    exercise controller completed by Steve

--- Controllers/ExercisesController.cs
+++ Controllers/ExercisesController.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
-using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
 using StudentExercises.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace StudentExercises.Controllers
 {
@@ -30,42 +31,36 @@ namespace StudentExercises.Controllers
             }
         }
 
-        // GET api/values
+        // GET api/exercise?language=JavaScript
         [HttpGet]
         public async Task<IActionResult> Get(string language)
         {
-
             using (IDbConnection conn = Connection)
             {
                 string sql = "SELECT * FROM Exercise";
 
                 if (language != null)
                 {
-                    sql += $" WHERE Language = '{language}'";
+                    sql += $" WHERE Language='{language}'";
                 }
 
-                var fullExercises = await conn.QueryAsync<Exercise>(
-                    sql);
+                var fullExercises = await conn.QueryAsync<Exercise>(sql);
                 return Ok(fullExercises);
             }
+
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetExercise")]
         public async Task<IActionResult> Get([FromRoute] int id)
         {
             using (IDbConnection conn = Connection)
             {
-                string sql = $"SELECT * FROM Exercise WHERE ID = {id}";
+                string sql = $"SELECT * FROM Exercise WHERE Id = {id}";
 
                 var theSingleExercise = (await conn.QueryAsync<Exercise>(sql)).Single();
                 return Ok(theSingleExercise);
             }
-        }
-
-        private void Single()
-        {
-            throw new NotImplementedException();
         }
 
         // POST api/values
@@ -84,18 +79,69 @@ namespace StudentExercises.Controllers
                 exercise.Id = newExerciseId;
                 return CreatedAtRoute("GetExercise", new { id = newExerciseId }, exercise);
             }
+
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Exercise exercise)
         {
+            string sql = $@"
+            UPDATE Exercise
+            SET Name = '{exercise.Name}',
+                Language = '{exercise.Language}'
+            WHERE Id = {id}";
+
+            try
+            {
+                using (IDbConnection conn = Connection)
+                {
+                    int rowsAffected = await conn.ExecuteAsync(sql);
+                    if (rowsAffected > 0)
+                    {
+                        return new StatusCodeResult(StatusCodes.Status204NoContent);
+                    }
+                    throw new Exception("No rows affected");
+                }
+            }
+            catch (Exception)
+            {
+                if (!ExerciseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete([FromRoute] int id)
         {
+            string sql = $@"DELETE FROM Exercise WHERE Id = {id}";
+
+            using (IDbConnection conn = Connection)
+            {
+                int rowsAffected = await conn.ExecuteAsync(sql);
+                if (rowsAffected > 0)
+                {
+                    return new StatusCodeResult(StatusCodes.Status204NoContent);
+                }
+                throw new Exception("No rows affected");
+            }
+
+        }
+
+        private bool ExerciseExists(int id)
+        {
+            string sql = $"SELECT Id, Name, Language FROM Exercise WHERE Id = {id}";
+            using (IDbConnection conn = Connection)
+            {
+                return conn.Query<Exercise>(sql).Count() > 0;
+            }
         }
     }
 }
